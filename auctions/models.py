dd3bc17e8dec1a26fe3dbc5b01ee513f6a20ae22
leny133 <63651131+leny133@users.noreply.github.com>
2021-01-17 18:17:08 -0400

    created models and fields

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class listings(models.Model):
+    title = models.CharField(max_length=140)
+    description = models.CharField(max_length=250)
+    active = models.BooleanField()
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.CharField(max_length=24)
+    image = models.URLField()
+
+class bids(models.Model):
+    auction = models.ForeignKey(listings, on_delete= models.PROTECT)
+    bidder = models.ForeignKey(User, on_delete= models.PROTECT, related_name= "bidder")
+    bidprice = models.DecimalField(decimal_places=2, max_digits=8)
+    winner = models.ForeignKey(User, null=True, related_name="winner", blank = True, on_delete= models.PROTECT)
+
+class comments(models.Model):
+    commentUser = models.ForeignKey(User, on_delete= models.PROTECT)
+    listingComment = models.ForeignKey(listings, on_delete= models.PROTECT)
+    comment = models.CharField(max_length=250)
